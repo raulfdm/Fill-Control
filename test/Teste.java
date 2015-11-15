202056f3b78b3300037e6c79ef4e05c967755492
raulfdm <melo.raulf@gmail.com>
2015-11-15 19:25:09 -0200

    Classe de Teste

---
+++ test/Teste.java
@@ -0,0 +1,23 @@
+
+import br.com.engsoft.controll.guicheModel;
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Raul
+ */
+public class Teste {
+
+    public static void main(String[] args) {
+        
+        String guiche = "ga01";
+        
+        System.out.println(guiche.substring(1, 2));
+    }
+
+}
