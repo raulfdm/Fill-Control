82ca62467711dee2d9163e879b0d4d89ca3d6220
raulfdm <melo.raulf@gmail.com>
2015-11-20 23:00:44 -0200

    Implementada a classe que fará o controle da Fila

---
+++ src/br/com/engsoft/controll/ControleDeFila.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.engsoft.controll;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Raul
+ */
+public class ControleDeFila {
+    String senhaPreferencial;
+    String senhaNormal;
+    ArrayList<String> fila = new ArrayList();
+    
+    public ControleDeFila() {
+    }
+    
+    public String gerarSenhaPreferencial(){
+        String senhaGerada = null;
+        
+        
+        
+        
+        return senhaGerada;
+    }
+}
