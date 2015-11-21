f6d9e0379c7d535b7c42991bde15120bca37e961
raulfdm <melo.raulf@gmail.com>
2015-11-20 23:02:08 -0200

    Implementado a classe que altera os botões de pausa

---
+++ src/br/com/engsoft/utils/AlteraImagens.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.engsoft.utils;
+
+import br.com.engsoft.main.AtendenteView;
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Raul
+ */
+public class AlteraImagens {
+
+    public ImageIcon continueIcon() {
+        try {
+//pacote onde estao as imagens no seu projeto
+            String diretorio = "/br/com/engsoft/img/continue.png";
+            URL resource = getClass().getResource(diretorio);
+            File file = new File(resource.toURI());
+            //setando o icone
+            ImageIcon logo = new ImageIcon(file.getPath());
+
+            return logo;
+
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(AtendenteView.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    public ImageIcon pauseIcon() {
+        try {
+            //pacote onde estao as imagens no seu projeto
+            String diretorio = "/br/com/engsoft/img/pause.png";
+            URL resource = getClass().getResource(diretorio);
+            File file = new File(resource.toURI());
+            //setando o icone
+            ImageIcon logo = new ImageIcon(file.getPath());
+
+            return logo;
+
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(AtendenteView.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return null;
+    }
+}
