3399d627e2465ff437b062293de6731e0477ccc6
raulfdm <melo.raulf@gmail.com>
2015-11-23 07:24:33 -0200

    Adicionada classe de utilitarios

---
+++ src/br/com/engsoft/utils/utilitarios.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.engsoft.utils;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ *
+ * @author Raul
+ */
+public class utilitarios {
+    
+    
+    public String dataAtual() {
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date today = Calendar.getInstance().getTime();
+        String reportDate = df.format(today);
+
+        return reportDate;
+    }
+    
+    
+
+   
+}
